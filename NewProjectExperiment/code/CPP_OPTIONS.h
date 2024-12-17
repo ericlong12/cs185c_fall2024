ea2611059b36a2700eb95561e29b591c0d5ab5a8
ericlong12 <114244136+ericlong12@users.noreply.github.com>
2024-12-17 03:43:10 -0800

    Delete NewProjectExperiment directory

--- NewProjectExperiment/code/CPP_OPTIONS.h
+++
@@ -1,9 +0,0 @@
-#ifndef CPP_OPTIONS_H
-#define CPP_OPTIONS_H
-
-#define SHORTWAVE_HEATING       /* Surface forcing for simplicity */
-#define NONLIN_FRSURF           /* Non-linear free surface (for wave propagation) */
-#define EXACT_CONSERV           /* Ensure mass conservation */
-#define INCLUDE_PHIHYD_CALCULATION_CODE
-
-#endif /* CPP_OPTIONS_H */
--- NewProjectExperiment/code/DIAGNOSTICS_SIZE.h
+++
@@ -1,2 +0,0 @@
-      INTEGER numDiags, diagSt_size
-      PARAMETER( numDiags = 20, diagSt_size = 20 )
